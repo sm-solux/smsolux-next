f823edd254136826c3bc14adeda9cc1ebffb618e
sehee0207 <lolry0207@gmail.com>
2026-02-17 23:12:55 +0900

    feat: 로딩 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-[#0F1012] z-50 fixed inset-0">
+            <div className="w-10 h-10 border-4 border-white/10 border-t-[#8CE0F4] rounded-full animate-spin"></div>
+        </div>
+    );
+}
